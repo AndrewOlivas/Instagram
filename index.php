8db0cf81e88177fe41c10a3b80d911dd29fc3581
Andrew Olivas <andrewrolivas@gmail.com>
2015-05-07 08:48:05 -0700

    milestone 2 done

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-index
+<?php
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+
+define('clientID', '286f5d5cab964acea2333b38249165d2'); 
+define('clientSecret', '60bc0c4f4b3a4c3a97583145f696ede2'); 
+define('redirectURI', 'https://localhost/appacademyapi/index.php'); 
+define('ImageDirectory', 'pics/');
+
+?>
+
+CLIENT INFO
+CLIENT ID	cda6ee6664f9482e8b14c0695678c53a
+CLIENT SECRET	31ae98225a2040308165d350229869c8
+WEBSITE URL	http://localhost;8888/instagram/index.php
+REDIRECT URI	http://localhost;8888/instagram/index.php
