486f0116912a601090cc09cade97b7784c836e27
Andrew Olivas <andrewrolivas@gmail.com>
2015-05-18 10:06:18 -0700

    milestone 12-13 done (finished by friday wouldn't let me push bc of folder change)

--- index.php
+++ index.php
@@ -3,35 +3,53 @@ set_time_limit(0);
 ini_set('default_socket_timeout', 300);
 session_start();
 
-define('clientID', '286f5d5cab964acea2333b38249165d2'); 
+define('clientID', 'cda6ee6664f9482e8b14c0695678c53a'); 
 define('clientSecret', '60bc0c4f4b3a4c3a97583145f696ede2'); 
-define('redirectURI', 'https://localhost:8888/appacademyapi/index.php'); 
+define('redirectURI', 'http://localhost/instagram/index.php'); 
 define('ImageDirectory', 'pics/');
 
 function connectToInstagram($url){
 	$ch = curl_init();
 
-	curl_setopt_array($ch, array[
-		CURLOPT_URL => $url;
-		CURLOPT_RETURNTRANSFER => true;
-		CURLOPT_SSL_VERIFYPEER => false;
-		CURLOPT_SSL_VERIFYHOST => $url;
-		]);
+	curl_setopt_array($ch, array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_SSL_VERIFYHOST => $url,
+		));
 	$result = curl_exec($ch);
 	curl_close($ch);
 	return $result;
 }
 
 function getUserID($userName){
-	$url = 'https://api.instagram.com/v1/users/search?q='.$userName._'&client_id'.clientID;
+	$url = 'https://api.instagram.com/v1/users/search?q='.$userName. '&client_id='.clientID;
 	$instagramInfo = connectToInstagram($url);
 	$results = json_decode($instagramInfo, true);
 
-	echo $results['data']['0']['id'];
+	echo $results['data'][0]['id'];
 }
 
+function printImages($userID){
+	$url = 'https://api.instagram.com/v1/users/'. $userID . '/media/recent?client_id='.clientID.'&count=10';
+	$instagramInfo = connectToInstagram($url);
+	$results = json_decode($instagramInfo, true);
+	// require_once(__DIR__ . "../view/header.php");
+	foreach ($results['data'] as $items){
+		$image_url = $items['images']['low_resolution']['url'];
+		echo '<img id="pics" src=" '.$image_url.' "/><br/>';
+		savePictures($image_url);
+	}	
+// require_once(__DIR__ . "../view/footer.php");
+}
+function savePictures($image_url){
+	$filename = basename($image_url); 
+		echo "<form action='$image_url'> <input type='submit' value='Fullscreen'></form>". '<br>';
+	$destination = ImageDirectory . $filename;
+		file_put_contents($destination, file_get_contents($image_url));
+}
 if (isset($_GET['code'])){
-	$code = ($_GET['code']);
+	$code = $_GET['code'];
 	$url = 'https://api.instagram.com/oauth/access_token';
 	$access_token_settings = array('client_id' => clientID,
 									'cliend_secret' => clientSecret,
@@ -50,7 +68,12 @@ $result = curl_exec($curl);
 curl_close($curl);
 
 $result = json_decode($result, true);
+
 echo $results['user']['username'];
+
+$userID = getUserID($userName);
+
+printImages($userID);
 }
 else {
 ?>
@@ -66,7 +89,7 @@ REDIRECT URI	http://localhost;8888/instagram/index.php -->
 		<meta charset="utf-8">
 		<meta name="decription" context="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Untitled</title>
+		<title>Instagram</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css"> 
 		<link rel="author" href="humans.txt">
 	</head>
@@ -76,3 +99,6 @@ REDIRECT URI	http://localhost;8888/instagram/index.php -->
 	<script type="text/javascript" src="js/main.js"></script>
 	</body>
 </html>
+<?php
+}
+?>
